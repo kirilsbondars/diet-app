d30d118cf63fbfe1fea5609056a3720e098fbd93
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2023-12-11 22:09:25 +0200

    email unique and nullable

--- src/models/models.py
+++ src/models/models.py
@@ -13,7 +13,7 @@ meals = db.Table('user_meal',
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(250), nullable=False, unique=True)
+    email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
 
     name = db.Column(db.String(250), nullable=False)
