197ad07026765988ff8b304a247302c265a0b20d
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2023-12-11 22:10:33 +0200

    jsonify deleted

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, jsonify
+from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 from flask_bcrypt import Bcrypt
 
