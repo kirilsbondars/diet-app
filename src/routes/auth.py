fac834b514a72113e219dc1191a65bb514f7d684
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2023-12-13 16:45:23 +0200

    required login for profile page

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
+from flask_login import login_required
 from src.controllers.auth import profile, sing_up, login, logout
 
 auth_blueprint = Blueprint('auth', __name__)
 
-
-auth_blueprint.route("/profile", methods=["GET", "POST"])(profile)
+auth_blueprint.route("/profile", methods=["GET", "POST"])(login_required(profile))
 auth_blueprint.route('/sing_up', methods=["GET", "POST"])(sing_up)
 auth_blueprint.route("/login", methods=["GET", "POST"])(login)
 auth_blueprint.route("/logout")(logout)
