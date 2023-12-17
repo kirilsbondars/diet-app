27683e1eb01aa9aad4211eda645bb645e225c5ce
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2023-12-17 19:34:21 +0200

    age to date_of_birth in sing_up, db, login, auth

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 from src.models.models import db, User
 
@@ -12,7 +13,7 @@ def profile():
         name = request.form.get("name")
         surname = request.form.get("surname")
         gender = request.form.get("gender")
-        age = request.form.get("age")
+        date_of_birth = datetime.strptime(request.form.get("date_of_birth"), "%Y-%m-%d").date()
         weight = request.form.get("weight")
         height = request.form.get("height")
         gluten_free = request.form.get("gluten-free") == 'on'
@@ -23,7 +24,7 @@ def profile():
         current_user.name = name
         current_user.surname = surname
         current_user.gender = gender
-        current_user.age = age
+        current_user.date_of_birth = date_of_birth
         current_user.weight = weight
         current_user.height = height
         current_user.gluten_free = gluten_free
@@ -31,7 +32,7 @@ def profile():
         current_user.vegetarian = vegetarian
         current_user.dairy_free = dairy_free
 
-        calories, fats, proteins, carbohydrates = calculate_nutrients(weight, height, age, gender)
+        calories, fats, proteins, carbohydrates = calculate_nutrients(weight, height, date_of_birth, gender)
         current_user.calories = calories
         current_user.fats = fats
         current_user.proteins = proteins
@@ -56,7 +57,7 @@ def sing_up():
         password = request.form.get("password")
         confirm_password = request.form.get("confirm-password")
         gender = request.form.get("gender")
-        age = request.form.get("age")
+        date_of_birth = datetime.strptime(request.form.get("date_of_birth"), "%Y-%m-%d").date()
         weight = request.form.get("weight")
         height = request.form.get("height")
         gluten_free = request.form.get("gluten-free") == 'on'
@@ -71,7 +72,7 @@ def sing_up():
             "password": password,
             "confirm_password": confirm_password,
             "gender": gender,
-            "age": age,
+            "date_of_birth": date_of_birth,
             "weight": weight,
             "height": height,
             "gluten_free": gluten_free,
@@ -91,9 +92,9 @@ def sing_up():
             return render_template('auth/sign_up.html', errors=errors, form_data=form_data)
 
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        calories, fats, proteins, carbohydrates = calculate_nutrients(weight, height, age, gender)
+        calories, fats, proteins, carbohydrates = calculate_nutrients(weight, height, date_of_birth, gender)
         user = User(name=name, surname=surname, email=email, password=hashed_password, gender=gender,
-                    age=age, weight=weight, height=height, gluten_free=gluten_free, vegan=vegan,
+                    date_of_birth=date_of_birth, weight=weight, height=height, gluten_free=gluten_free, vegan=vegan,
                     vegetarian=vegetarian, dairy_free=dairy_free, calories=calories, fats=fats, proteins=proteins, carbohydrates=carbohydrates)
         db.session.add(user)
         db.session.commit()
@@ -116,7 +117,14 @@ def logout():
     return redirect(url_for("index.render_page_index"))
 
 
-def calculate_nutrients(weight, height, age, gender):
+def calculate_nutrients(weight, height, date_of_birth, gender):
+    weight = float(weight)
+    height = float(height)
+
+    # Calculate age
+    today = datetime.today()
+    age = today.year - date_of_birth.year - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
+
     if gender == "male":
         calories = (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)) * 1.375
     else:
@@ -129,5 +137,11 @@ def calculate_nutrients(weight, height, age, gender):
     fats = (fat_percentage / 100) * calories / 9
     proteins = (protein_percentage / 100) * calories / 4
     carbohydrates = (carb_percentage / 100) * calories / 4
+
+    # Round the values
+    calories = round(calories, 2)
+    fats = round(fats, 2)
+    proteins = round(proteins, 2)
+    carbohydrates = round(carbohydrates, 2)
 
     return calories, fats, proteins, carbohydrates
