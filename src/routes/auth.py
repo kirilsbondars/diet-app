b1e88c1b74ccd429abdedcbbbeaf74c719e77a10
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2023-12-21 17:19:29 +0200

    auth, create menu links

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -4,7 +4,7 @@ from src.controllers.auth import profile, sing_up, login, logout
 
 auth_blueprint = Blueprint('auth', __name__)
 
+auth_blueprint.route("/", methods=["GET", "POST"])(login)
 auth_blueprint.route("/profile", methods=["GET", "POST"])(login_required(profile))
 auth_blueprint.route('/sing_up', methods=["GET", "POST"])(sing_up)
-auth_blueprint.route("/login", methods=["GET", "POST"])(login)
 auth_blueprint.route("/logout")(logout)
--- src/routes/index.py
+++ src/routes/index.py
@@ -3,4 +3,4 @@ from src.controllers.index import render_page_index
 
 index_blueprint = Blueprint('index', __name__)
 
-index_blueprint.route('/', methods=['GET'])(render_page_index)
+index_blueprint.route('/index', methods=['GET'])(render_page_index)
