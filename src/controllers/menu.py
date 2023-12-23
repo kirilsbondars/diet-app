0b11712f4ae0564fe24a91384b76cb9eaacfbdb2
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2023-12-23 12:13:04 +0200

    meal algorithm

--- src/controllers/menu.py
+++ src/controllers/menu.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from pulp import LpProblem, LpMinimize, LpVariable, lpSum
 from src.models.models import Meal, user_meal, db
 from datetime import datetime
@@ -6,40 +6,50 @@ from sqlalchemy import insert
 from flask_login import current_user
 
 
-def view_menu():
-    return render_template('menu/create-menu.html')
+def create_menu_view():
+    if request.method == 'POST':
+        date = datetime.strptime(request.form['date'], '%Y-%m-%d').date()
+        user = current_user
+
+        user_preferences = get_user_preferences(user)
+
+        # gets meals
+        meals = Meal.query.filter_by(**user_preferences).all()
+
+        # error message if no meals found
+        if not meals:
+            flash('No meals found that match your preferences.', 'error')
+            return render_template('menu/create-menu.html', date=date)
+
+        # creates menus
+        if meals:
+            meals_portions = create_menu(meals, user.calories, user.fats, user.proteins,
+                                         user.carbohydrates)
+            delete_menu(user.id, date)
+            save_menu(meals_portions, user.id, date, 'Breakfast')
+            db.session.commit()
+
+            return redirect(url_for('menu.history', date=date))
+
+    return render_template('menu/create-menu.html', today=datetime.today().date())
 
 
-def create_one_day_menu(date=datetime.today().date()):
-    # delete current menu for this day
-    delete_menu(current_user.id, date)
-
+def create_menu_for_one_day(user, date):
+    pass
     # nutrients per mealtime
-    calories = {'Breakfast': 800, 'Lunch': 800, 'Dinner': 800}
-    fats = {'Breakfast': 20, 'Lunch': 30, 'Dinner': 20}
-    proteins = {'Breakfast': 20, 'Lunch': 30, 'Dinner': 20}
-    carbohydrates = {'Breakfast': 100, 'Lunch': 150, 'Dinner': 100}
+    # calories = {'Breakfast': 800, 'Lunch': 800, 'Dinner': 800}
+    # fats = {'Breakfast': 20, 'Lunch': 30, 'Dinner': 20}
+    # proteins = {'Breakfast': 20, 'Lunch': 30, 'Dinner': 20}
+    # carbohydrates = {'Breakfast': 100, 'Lunch': 150, 'Dinner': 100}
 
-    # get user preferences
-    user_preferences = {attr: getattr(current_user, attr) for attr in
+
+def get_user_preferences(user):
+    user_preferences = {attr: getattr(user, attr) for attr in
                         ['gluten_free', 'vegan', 'vegetarian', 'dairy_free']}
-    # if a preference is False, remove it from the preferences
     for preference, value in list(user_preferences.items()):
         if not value:
             del user_preferences[preference]
-
-    # create menu for breakfast
-    meals = Meal.query.filter_by(
-        **user_preferences  # filter meals based on user preferences
-    ).all()
-    if meals:
-        meals_portions = create_menu(meals,
-                                     float(current_user.calories),
-                                     float(current_user.fats),
-                                     float(current_user.proteins),
-                                     float(current_user.carbohydrates))
-        save_menu(meals_portions, current_user.id, date, 'Breakfast')
-        db.session.commit()
+    return user_preferences
 
 
 def delete_menu(user_id, date):
@@ -55,27 +65,20 @@ def save_menu(meals_portions, user_id, date, mealtime):
 
 
 def create_menu(meals, calories, fats, proteins, carbohydrates):
-    # Use the provided nutritional requirements
-    user_requirements = {
-        'calories': calories,
-        'fats': fats,
-        'proteins': proteins,
-        'carbohydrates': carbohydrates
-    }
-
     # Define the problem
     prob = LpProblem("OptimalMenu", LpMinimize)
 
     # Define the decision variables
-    meal_vars = LpVariable.dicts("Meals", [meal.id for meal in meals], 0, 3)
+    meal_vars = LpVariable.dicts("Meals", [meal.id for meal in meals], 0)
 
     # Define the objective function
-    prob += lpSum([meal_vars[meal.id] * float(meal.price) for meal in meals])
+    prob += lpSum([meal_vars[meal.id] * meal.price for meal in meals])
 
     # Define the constraints
-    for nutrient in ['calories', 'fats', 'proteins', 'carbohydrates']:
-        prob += lpSum([meal_vars[meal.id] * float(getattr(meal, nutrient)) for meal in meals]) >= user_requirements[
-            nutrient]
+    prob += calories * 1.1 >= lpSum([meal_vars[meal.id] * getattr(meal, 'calories') for meal in meals]) >= calories * 0.9
+    prob += fats * 1.1 >= lpSum([meal_vars[meal.id] * getattr(meal, 'fats') for meal in meals]) >= fats * 0.9
+    prob += proteins * 1.1 >= lpSum([meal_vars[meal.id] * getattr(meal, 'proteins') for meal in meals]) >= proteins * 0.9
+    prob += carbohydrates * 1.1 >= lpSum([meal_vars[meal.id] * getattr(meal, 'carbohydrates') for meal in meals]) >= carbohydrates * 0.9
 
     # Solve the problem
     prob.solve()
