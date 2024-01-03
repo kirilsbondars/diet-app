665ffe982c32d8c91db89a4843c04d9caad760a4
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2024-01-03 18:01:29 +0200

    path fix

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap5
