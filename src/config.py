aa34cc60d99dcdfb4994f43cfe3aa0b76c4b498a
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2024-01-05 13:51:53 +0200

    deployment fix, db

--- src/config.py
+++ src/config.py
@@ -7,6 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/diet-app'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://diet_app_user:123@localhost/diet_app'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
