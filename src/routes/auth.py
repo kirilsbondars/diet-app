602a4e7c5e2a0afa686e9f2145743e3398a9f070
kirilsbondars <49512619+kirilsbondars@users.noreply.github.com>
2024-01-11 17:11:40 +0200

    start page

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,9 +1,16 @@
-from flask import Blueprint
-from flask_login import login_required
+from flask import Blueprint, redirect, url_for
+from flask_login import login_required, current_user
 
 from controllers.auth import profile, sing_up, login, logout
 
 auth_blueprint = Blueprint('auth', __name__)
+
+@auth_blueprint.route("/", methods=["GET", "POST"])
+def start_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('menu.create_menu_view'))
+    else:
+        return login()
 
 auth_blueprint.route("/", methods=["GET", "POST"])(login)
 auth_blueprint.route("/profile", methods=["GET", "POST"])(login_required(profile))
